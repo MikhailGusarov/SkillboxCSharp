f0be0e1036670df6ff6ca6f7d1ef774154290dee
GusarovMV <vgg639@gmail.com>
2020-10-07 16:49:47 +0300

    дописал структура записей + реализовал вывод данных по записи в структуре ежедневника

--- Module07/Theme_07/Homework_07/Diary.cs
+++ Module07/Theme_07/Homework_07/Diary.cs
@@ -8,6 +8,21 @@ namespace Homework_07
 {
     struct Diary
     {
+        private Record[] Records;
 
+        public string this[DateTime dateTime]
+        {
+            get
+            {
+                for (int i = 0; i < Records.Length; i++)
+                {
+                    if (Records[i].Date + Records[i].Time == dateTime || Records[i].Date == dateTime)
+                    {
+                        return Records[i].GetInfoRecord();
+                    }
+                }
+                return "На данную дату ничего не запланированно!";
+            }
+        }
     }
 }
--- Module07/Theme_07/Homework_07/Record.cs
+++ Module07/Theme_07/Homework_07/Record.cs
@@ -35,22 +35,61 @@ namespace Homework_07
         /// Дата
         /// </summary>
         public DateTime Date { get { return this.date; } set { this.date = value.Date; } }
-
         /// <summary>
         /// Время
         /// </summary>
         public TimeSpan Time { get { return this.time; } set { this.time = value; } }
-
         /// <summary>
         /// Заголовок записи
         /// </summary>
         public string Header { get { return this.header; } set { this.header = value; } }
-
         /// <summary>
         /// Сождержание записи
         /// </summary>
         public string Body { get { return this.body; } set { this.body = value; } }
-
+        /// <summary>
+        /// Статус завершения 
+        /// </summary>
         public bool IsDone { get { return this.isDone; } set { this.isDone = value; } }
+
+        public Record(DateTime date, TimeSpan time, string header, string body, bool isDone)
+        {
+            this.date = date;
+            this.time = time;
+            this.header = header;
+            this.body = body;
+            this.isDone = isDone;
+        }
+        public Record(DateTime date, TimeSpan time, string header, string body):
+            this(date, time, header, body, false)
+        {
+
+        }
+        public Record(DateTime date, TimeSpan time, string header) :
+            this(date, time, header, "", false)
+        {
+
+        }
+        public Record(DateTime date, string header) :
+            this(date, TimeSpan.Zero, header, "", false)
+        {
+
+        }
+        public Record(DateTime date, string header, string body) :
+            this(date, TimeSpan.Zero, header, body, false)
+        {
+
+        }
+
+        /// <summary>
+        /// Вывод информации о записи на экран
+        /// </summary>
+        /// <returns>Строка с информацией о записи</returns>
+        public string GetInfoRecord()
+        {
+            // TODO реализовать
+            return "";
+        }
+
     }
 }
